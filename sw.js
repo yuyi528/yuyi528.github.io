17db20b342a1be0869cf469acbddff18680fe926
yuyi528 <zya7896321@126.com>
2025-02-16 01:32:55 +0800

    Site updated: 2025-02-16 01:32:49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739631027012";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739640718135";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
